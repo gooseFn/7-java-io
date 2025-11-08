27c7d2337a55a8dfc346363d58272844bae1117a
gooseFn <oleg13052006@gmail.com>
2025-11-09 00:42:04 +0500

    done

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -9,5 +9,30 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+        int currentByte;
+        boolean lastWasCarriageReturn = false;
+        while((currentByte = System.in.read()) != -1) {
+            if (currentByte == 13) {
+                if (lastWasCarriageReturn) System.out.write(13);
+                lastWasCarriageReturn = true;
+            }
+            else if (currentByte == 10) {
+                if (lastWasCarriageReturn) {
+                    System.out.write(10);
+                    lastWasCarriageReturn = false;
+                }
+                else System.out.write(10);
+            }
+            else {
+                if (lastWasCarriageReturn) {
+                    System.out.write(13);
+                    lastWasCarriageReturn= false;
+                }
+                System.out.write(currentByte);
+            }
+        }
+
+        if (lastWasCarriageReturn) System.out.write(13);
+        System.out.flush();
     }
 }
